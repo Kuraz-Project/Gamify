af8014c5d3bb5ff661878552b6dd0650276a82fd
Samuel D. Tadesse <samueldereje1996@gmail.com>
2025-09-16 01:19:21 +0400

    - Added React Router DOM

--- gamify/src/App.jsx
+++ gamify/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-import NavBar from './Components/NavBar'
+import {Route, createBrowserRouter, RouterProvider, createRoutesFromElements} from 'react-router-dom'
+import MainLayout from './Pages/MainLayout'
+import Home from './Pages/Home'
+import Explore from './Pages/Explore'
+import Leader from './Pages/Leader'
+import Create from './Pages/Create'
+import Error from './Pages/Error'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path = "/" element = {<MainLayout />}>
+      <Route index element={<Home />}/> 
+      <Route path='explore' element={<Explore />}/>
+      <Route path='leader' element={<Leader />}/>
+      <Route path='create' element={<Create />}/>
+      <Route path="*" element={<Error />}/>
+    </Route>
+  )
+)
 
 const App = () => {
   return (
-    <NavBar />
+    <RouterProvider router={router}/>
   )
 }
 
--- gamify/src/Pages/MainLayout.jsx
+++ gamify/src/Pages/MainLayout.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import NavBar from '../Components/NavBar'
+import { Outlet } from 'react-router-dom'
+import Footer from '../Components/Footer'
 
 const MainLayout = () => {
   return (
-    <div>MainLayout</div>
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
   )
 }
 
